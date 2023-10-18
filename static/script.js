3ce5fb4eb08ee78385620976e007c380e140f0ce
Zakarias Bergcrona <zb222cw@student.lnu.se>
2023-10-18 15:54:27 +0200

    Adds more js in script.js

--- static/script.js
+++ static/script.js
@@ -6,3 +6,46 @@ function getWeatherData() {
         showErrorMessage("Please enter a valid city")
     }
 }
+
+async function getWeather(city) {
+ try {
+   const response = await fetch('http://localhost:3000/api/weather', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ city: city})
+   })
+
+   if (!response.ok) {
+    throw new Error('No Network Response')
+   }
+
+   const data = await response.json()
+   displayWeatherData(data)
+} catch (error) {
+    console.error('There was a problem with the fetch operation')
+}
+
+}
+
+function displayWeatherData (data) {
+    document.getElementById('cityPrompt').style.display = 'none'
+
+    const weatherOutput = document.getElementById('weatherOutput')
+    weatherOutput.innerHTML = `
+        <h2>Weather in ${data.city}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Conditions: ${data.weather[0].description}</p>
+
+        <button onclick="resetView()">Go Back</button>
+        <button onclick="showCharts()">Show charts</button>
+        
+    `
+    weatherOutput.style.display = 'block'
+}
+
+function resetView() {
+    document.getElementById('cityPrompt').style.display = 'block'
+    document.getElementById('weatherOutput').style.display = 'none'
+}
